66834a7519bf7af5f8b5a17e7e01aea91dcb18cd
GonzalezNacho <nacho.ldm63@gmail.com>
2021-11-15 11:54:46 -0300

    detalle del producto cambiar foto

---
+++ js/detalleProductos.js
@@ -0,0 +1,9 @@
+function cambiarImagenAContratapa() {
+    var fotoPrincipal = document.getElementById("foto-principal");
+    fotoPrincipal.src = "./assets/img/beat_revolver2.jpg";
+}
+
+function cambiarImagenATapa() {
+    var fotoPrincipal = document.getElementById("foto-principal");
+    fotoPrincipal.src = "./assets/img/Revolver.jpg";
+}
