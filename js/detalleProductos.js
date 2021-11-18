f8972d24f20a8f2e641b9a3a98b7cb0ad6c5ff4e
GonzalezNacho <nacho.ldm63@gmail.com>
2021-11-18 13:58:01 -0300

    detalle del producto

--- js/detalleProductos.js
+++ js/detalleProductos.js
@@ -7,3 +7,13 @@ function cambiarImagenATapa() {
     var fotoPrincipal = document.getElementById("foto-principal");
     fotoPrincipal.src = "./assets/img/Revolver.jpg";
 }
+
+function crearComentario() {
+    let comentario = document.getElementById("textAreaComentario").value
+    let email = document.getElementById("emailComentario").value
+    let nombre = document.getElementById("nombreComentario").value
+    console.log(comentario)
+    console.log(email)
+    console.log(nombre)
+    const divisionComment = document.querySelector('.conteiner-comentarios')
+}
