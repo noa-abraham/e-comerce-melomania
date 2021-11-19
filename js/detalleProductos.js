fa63194f709b84f6c02159f9e4c582fe73d0e2a7
GonzalezNacho <nacho.ldm63@gmail.com>
2021-11-18 22:43:24 -0300

    detalle del producto javascript

--- js/detalleProductos.js
+++ js/detalleProductos.js
@@ -7,13 +7,50 @@ function cambiarImagenATapa() {
     var fotoPrincipal = document.getElementById("foto-principal");
     fotoPrincipal.src = "./assets/img/Revolver.jpg";
 }
+let cantidadEstrellas = 0;
+let emailCorrecto = 0;
 
 function crearComentario() {
-    let comentario = document.getElementById("textAreaComentario").value
-    let email = document.getElementById("emailComentario").value
-    let nombre = document.getElementById("nombreComentario").value
-    console.log(comentario)
-    console.log(email)
-    console.log(nombre)
-    const divisionComment = document.querySelector('.conteiner-comentarios')
+    let comentario = document.getElementById("textAreaComentario").value;
+    let email = document.getElementById("emailComentario").value;
+    let nombre = document.getElementById("nombreComentario");
+    chequearMail(email);
+    if (!nombre.checkValidity()) {
+        alert('El campo no es válido.');
+    } else {
+        alert('El campo es válido.');
+    }
+    console.log(comentario);
+    console.log(email);
+    console.log(nombre);
+    const divisionComment = document.querySelector('.conteiner-comentarios');
+    let myDivComment = document.createElement('div');
+    myDivComment.classList.add("card", "col-md-5", "conteiner-lg", "valoraciones-realizadas");
+    let myDivCommentTittle = document.createElement('div');
+    myDivCommentTittle.classList.add("card-header", "titulo-comentario");
+    let myH6Autor = document.createElement('h6');
+    myH6Autor.classList.add("autor-del-comentario");
+    myH6Autor.textContent = nombre.value;
+    myDivCommentTittle.appendChild(myH6Autor);
+    myDivComment.appendChild(myDivCommentTittle);
+    let myDivCommentText = document.createElement('div');
+    myDivCommentText.classList.add("card-body");
+    let myPText = document.createElement('p');
+    myPText.classList.add("card-text");
+    myPText.textContent = comentario + cantidadEstrellas;
+    myDivCommentText.appendChild(myPText);
+    myDivComment.appendChild(myDivCommentText);
+    divisionComment.appendChild(myDivComment);
+}
+
+function estrellas(element) {
+    cantidadEstrellas = element.id;
+}
+
+function chequearMail(string) {
+    let posicionDelArroba = string.indexOf("@");
+    let posicionDelPunto = string.lastIndexOf(".");
+    if (posicionDelPunto > posicionDelArroba) {
+        emailCorrecto = 1;
+    }
 }
