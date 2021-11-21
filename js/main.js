7c58daa80ad9c4d3f341d7230e40ce1c108b3280
noa-abraham <noeliasabraham@gmail.com>
2021-11-21 18:26:04 -0300

    .\.git\

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-console.log("Hola mundo");
-
 const url = 'discos.json';
 const request = new XMLHttpRequest();
 request.open('GET', url); // setean el método, la url de api
 request.responseType = 'json'; //definen el tipo de dato que les devuelve
 request.send(); //envían la solicitud
+
 var discos = '';
 
-request.onload = fuction(); { //espera la respuesta
+request.onload = function() {
     console.log(request.response);
-    discos = request.response; //recibe la respuesta
+    discos = request.response; //le asigno la respuesta a una variable global
+    setTablaDestacados(); //para que aparezca determinada info de los destacados en las cards del home. 
+}
+
+function setTablaDestacados() {
+
 }
