3889e24b1c3908b2a282fa1bca4ce1f7e5a0e81e
noa-abraham <noeliasabraham@gmail.com>
2021-11-21 20:59:15 -0300

    .\.git\

--- js/main.js
+++ js/main.js
@@ -13,5 +13,23 @@ request.onload = function() {
 }
 
 function setTablaDestacados() {
+    let productos = discos.productos;
+    productos.forEach(element => {
 
+        let elementGalery = '<div class="col"><div class="card shadow-sm" id="card' + element.id + '">';
+        elementGalery += '<svg class="bd-placeholder-img card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">' + element.titulo + '</text></svg>';
+        elementGalery += '<img src= ' + element.tapa + 'alt="">';
+        elementGalery += '<div class="card-body">';
+        elementGalery += '<p class="card-text"><b> ARTISTA: </b>' + element.artista + ' </p>';
+        elementGalery += '<p class="card-text"><b> GÉNERO: </b>' + element.genero + ' </p>';
+        elementGalery += '<div class="d-flex justify-content-between align-items-center">';
+        elementGalery += '<div class="btn-group">';
+        elementGalery += '<button type="button" class="btn btn-sm btn-outline-secondary"> <a href="detalleProductos.html"> Detalle</a></button>';
+        elementGalery += '</div><small class="text-muted">9 mins</small></div></div></div></div>';
+
+        let gallery = document.getElementById('gallery_view');
+        $('#gallery_view').append(elementGalery);
+
+        console.log(element);
+    });
 }
