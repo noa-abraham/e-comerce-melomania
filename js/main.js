94a2e55f165ef3fe8eeef6a7184bf4ff3c3289f8
noa-abraham <noeliasabraham@gmail.com>
2021-11-21 13:49:07 -0300

    .\.git\

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+//Cargando las librerias Http... requets con Jquey
+
 function getData() {
     let url = "ajax/api.json";
-    $.getJSON(url, function(data) {
-            var items = [];
-            $.each(data, fuction(key, val) {
-                items.push("<li id ='" + key + "'>" + val + "</li>");
+    $.getJSON(url, function(data) { //procesa el data y devuelve la info que sigue
+                setDataJson(data);
 
-            });
+                .done(function() { //validaciones para ver si se cargo
+                    console.log("second success";)
+                })
 
-        }
+                .fail(function() {
+                    console.log(" error");
+                })
+            }
 
-    }
+            function setDataJson(data) {
+                var items = [];
+                $.each(data, fuction(key, val) {
+                    items.push("<li id ='" + key + "'>" + val + "</li>");
+                });
+
+                $("<ul/", {
+                    "class": "my-new-lits",
+                    html: items.join("")
+                }).appendTo("body");
+            }
