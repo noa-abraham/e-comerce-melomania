7a8ba329d6d36ca5fadde95497c1adeb6a5f39d9
GonzalezNacho <nacho.ldm63@gmail.com>
2021-11-22 08:54:06 -0300

    cambio en la funcion para agarrar un parametro de la url

--- js/scriptJSONDetalleProductos.js
+++ js/scriptJSONDetalleProductos.js
@@ -1,14 +1,4 @@
-/**
- * @param String name
- * @return String
- */
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-var prodId = getParameterByName('prodId');
-
-console.log(prodId);
+var urlEnString = window.location.href; 
+var url = new URL(urlEnString);
+var productId = url.searchParams.get("prodId");
+console.log("productID: " + productId);
